ac687d101864c455f05b4640d55d47efd5ccfd50
Wilondja243 <lucksonpremier@gmail.com>
2025-10-09 23:55:55 +0200

    update env

--- dealflow/config/production.py
+++ dealflow/config/production.py
@@ -11,8 +11,7 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(',')
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173", 
-    "http://127.0.0.1:5173",
+    "https://dealflow-9wex.onrender.com/",
 ]
 
 
@@ -40,8 +39,5 @@ SECURE_SSL_REDIRECT = config("SECURE_SSL_REDIRECT")
 SECURE_HSTS_SECONDS = config("SECURE_HSTS_SECONDS", cast = int)
 SECURE_HSTS_PRELOAD = config("SECURE_HSTS_PRELOAD")
 SECURE_HSTS_INCLUDE_SUBDOMAINS = config("SECURE_HSTS_INCLUDE_SUBDOMAINS")
-
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [ "https://waiteka.onrender.com",]
 
 
